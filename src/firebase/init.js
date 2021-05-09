e918d206525aa418f2d34ecdcd5d48a239df9a3b
Achilles45 <achillesusuoyibo7@gmail.com>
2021-05-09 07:28:19 +0100

    Changed firebase

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "AIzaSyBKexHeJk7HxdV_YnopTz3sTPGaJzF5VuY",
-  authDomain: "courier-app-86471.firebaseapp.com",
-  databaseURL: "https://courier-app-86471.firebaseio.com",
-  projectId: "courier-app-86471",
-  storageBucket: "courier-app-86471.appspot.com",
-  messagingSenderId: "360501964838",
-  appId: "1:360501964838:web:32a756588425066cecf6e7",
-  measurementId: "G-PTX7WLNRT8"
+  apiKey: "AIzaSyBwHmGLwsbNp4umFKnPGxkiLW4i4l-NlSI",
+  authDomain: "oceanflow-15c0d.firebaseapp.com",
+  projectId: "oceanflow-15c0d",
+  storageBucket: "oceanflow-15c0d.appspot.com",
+  messagingSenderId: "986938432040",
+  appId: "1:986938432040:web:67940338c35bba97ebf31d",
+  measurementId: "G-LZBTYRT3DM"
 };
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(firebaseConfig);
